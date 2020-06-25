2a6995adcb0a55949e9c527a399f63615c31ae39
rohanJa <rohan.jain150300@gmail.com>
2020-06-25 16:30:48 +0530

    Update about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,9 +3,14 @@ import React, { Fragment } from 'react'
 const About = () => {
     return (
         <Fragment>
-            <h1>About This App</h1>
-            <p>App to search Github users</p>
-            <p>Version:1.0.0</p>
+            <h1>About this App</h1>
+            <pre>
+                This web application help a user to find github repository of another <br></br>
+                user and more details about the other user activity on Github.<br></br>
+                <br></br><br></br>
+
+                <center>Made by <a href='https://github.com/rohanJa'target='_blank'>Rohan Jain</a></center>
+            </pre>
         </Fragment>
     )
 }
