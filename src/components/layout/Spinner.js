dbff23ddf57389035d44d6e63faf6673935dfec1
rohanJa <rohan.jain150300@gmail.com>
2020-06-25 16:30:23 +0530

    Indented spinner

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,10 +1,14 @@
-import React ,{Fragment} from 'react'
+import React ,{ Fragment } from 'react'
 import spinner from './spinner.gif'
 
 const Spinner = () => {
     return (
         <Fragment>
-            <img src={spinner} alt="Loading..." style={{width: '200px',margin: 'auto',display:'block'}} />
+            <img 
+                src={spinner} 
+                alt="Loading..." 
+                style={{width: '200px',margin: 'auto',display:'block'}} 
+            />
         </Fragment>
     )
 }
